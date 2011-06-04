5cac520faad10079ccd855925fd4816e3e04ad4a
gelidelf@gmail.com <gelidelf@gmail.com@21b69b27-b653-a999-551f-f9debfb54786>
2011-06-04 20:03:42 +0000

    Creadas las clases de Mensaje y de buzon para probar el commit y el update

---
+++ SIGonzalo/src/Messages/Inbox.java
@@ -0,0 +1,15 @@
+package Messages;
+
+import java.util.ArrayList;
+
+public class Inbox {
+
+	private ArrayList<Message> _inbox = null;
+	private ArrayList<Message> _priorityInbox = null;
+	
+	
+	public Inbox(){
+		
+		
+	}
+}
