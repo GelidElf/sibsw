8dd46c21991018a560ada019001280ab7ac0b1f1
GelidElf <GelidElf@21b69b27-b653-a999-551f-f9debfb54786>
2012-08-16 19:58:17 +0000

    Cambio de la vista de la montaje y añadidos los status panel de cada componente

--- trunk/src/core/gui/mainview/MainView.java
+++ trunk/src/core/gui/mainview/MainView.java
@@ -2,25 +2,35 @@ package core.gui.mainview;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class MainView extends JPanel {
 
 	private static final long serialVersionUID = -1370803844314118482L;
-	private static final String BACKGROUND_IMAGE = "images/VistaCompleta1.png";
+	private static final String BACKGROUND_IMAGE = "/images/VistaCompleta2.png";
+	private Image origBackGroundImage;
 	private Image backGroundImage;
 
 	public MainView() {
-		backGroundImage = Toolkit.getDefaultToolkit().createImage(BACKGROUND_IMAGE);
-		setSize(449, 327);
+		origBackGroundImage = new ImageIcon(MainView.class.getResource(BACKGROUND_IMAGE)).getImage();
+		backGroundImage = origBackGroundImage;
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent arg0) {
+				backGroundImage = origBackGroundImage.getScaledInstance(getWidth(), getHeight(), 5);
+			}
+		});
+		setVisible(true);
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(backGroundImage, 0, 0, this.getWidth(), this.getHeight(), null);
+		g.drawImage(backGroundImage, 0, 0, getWidth(), getHeight(), this);
 	}
 
 }
