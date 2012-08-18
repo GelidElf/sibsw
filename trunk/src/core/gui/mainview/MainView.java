9fb81846ff4ae32c5402b9dc603a525e6e835773
gelidelf <gelidelf@21b69b27-b653-a999-551f-f9debfb54786>
2012-08-18 16:04:12 +0000

    Extraemos la funcionalidad de mostrar una imagen como fondo a una clase propia y realizamos los cambios pertinentes en MainView

--- trunk/src/core/gui/mainview/MainView.java
+++ trunk/src/core/gui/mainview/MainView.java
@@ -1,36 +1,15 @@
 package core.gui.mainview;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import core.gui.imagepanel.JImagePanel;
 
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-
-public class MainView extends JPanel {
+public class MainView extends JImagePanel {
 
 	private static final long serialVersionUID = -1370803844314118482L;
 	private static final String BACKGROUND_IMAGE = "/images/VistaCompleta2.png";
-	private Image origBackGroundImage;
-	private Image backGroundImage;
 
 	public MainView() {
-		origBackGroundImage = new ImageIcon(MainView.class.getResource(BACKGROUND_IMAGE)).getImage();
-		backGroundImage = origBackGroundImage;
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent arg0) {
-				backGroundImage = origBackGroundImage.getScaledInstance(getWidth(), getHeight(), 5);
-			}
-		});
+		super(BACKGROUND_IMAGE);
 		setVisible(true);
 	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(backGroundImage, 0, 0, getWidth(), getHeight(), this);
-	}
-
+	
 }
