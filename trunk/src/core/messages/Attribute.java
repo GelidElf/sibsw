d7cdf5ac1ed1e7f35994ad73e25c55382efe0eb4
GelidElf <GelidElf@21b69b27-b653-a999-551f-f9debfb54786>
2012-08-23 20:31:09 +0000

    Comentarios en las clases de mensajes

--- trunk/src/core/messages/Attribute.java
+++ trunk/src/core/messages/Attribute.java
@@ -2,28 +2,36 @@ package core.messages;
 
 import java.io.Serializable;
 
-public class Attribute implements Serializable{
+/**
+ * Element class for each element in the contents of a message
+ * 
+ * Has a name, or identifier, that must be unique within a Message, and the value that could be any object
+ * 
+ * @author GelidElf
+ *
+ */
+public class Attribute implements Serializable {
 
 	private static final long serialVersionUID = 4891814832682990272L;
-	private String _name = null;
-	private Object _value = null;
-	
-	public Attribute (){
-		_name = new String("");
-		_value = new String("");
+	private String name = null;
+	private Object value = null;
+
+	public Attribute() {
+		name = new String("");
+		value = new String("");
 	}
-	
-	public Attribute (String name, Object value){
-		_name = name;
-		_value = value;
+
+	public Attribute(String name, Object value) {
+		this.name = name;
+		this.value = value;
 	}
-	
-	public String getName(){
-		return _name;
+
+	public String getName() {
+		return name;
 	}
-	
-	public Object getValue(){
-		return _value;
+
+	public Object getValue() {
+		return value;
 	}
-	
+
 }
--- trunk/src/core/messages/CommunicationManager.java
+++ trunk/src/core/messages/CommunicationManager.java
@@ -1,19 +1,77 @@
-	package core.messages;
+package core.messages;
 
 import core.messages.enums.CommunicationIds;
 
-
+/**
+ * This interface has the public accesible methods that all implementations of
+ * the communicaction managers must implement.
+ * 
+ * A communicacion manager is the element that will handle communication between
+ * automatas.
+ * 
+ * @author GelidElf
+ * 
+ */
 public interface CommunicationManager {
-	
-	public static final int DATAGRAM_LENGTH = 20000;
-	public static final int DEF_SERVER_PORT = 40000;
-	
+
+	/**
+	 * This method sends the message to the destination specified on the message
+	 * 
+	 * @param message
+	 *            the message to be sent
+	 */
 	public void sendMessage(Message message);
+
+	/**
+	 * Returns the inbox where this object is storing the messages received
+	 * 
+	 * @return the inbox for this communication manager
+	 */
 	public Inbox getInbox();
+
+	/**
+	 * Setter for the inbox in this communication manager, used mainly to
+	 * provide several communication managers with the same shared inbox
+	 * 
+	 * @param inbox
+	 *            the inbox to be setted
+	 */
 	public void setInbox(Inbox inbox);
+
+	/**
+	 * Returns the owner of this Communication element, this should be always
+	 * the Automata that created the object. This method is used to identify
+	 * messages for broadcast, identify ownership of received messages, create
+	 * speciffic behavoiur in the communication manager depending on who is the
+	 * father
+	 * 
+	 * @return the owner of this communication manager
+	 */
 	public CommunicationIds getOwner();
+
+	/**
+	 * This method must contain the initializing code for the manager. This
+	 * usually means, waiting for clients to connect, or trying to connect to
+	 * the server
+	 */
 	public void initialize();
+
+	/**
+	 * Places the message in the parameter in the inbox used by the
+	 * communication manager.
+	 * 
+	 * @param message
+	 *            the message to be inserted into the inbox
+	 */
 	public void feed(Message message);
-	public void clientDisconnected( CommunicationIds commId);
-	
+
+	/**
+	 * This method is called when a client is disconnected, it must implement
+	 * proper notification, code to wait for client to reconnect, or codeto try
+	 * to reconnect
+	 * 
+	 * @param commId
+	 */
+	public void clientDisconnected(CommunicationIds commId);
+
 }
