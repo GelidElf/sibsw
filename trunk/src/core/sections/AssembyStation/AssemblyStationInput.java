ac97fc4b000542e69314530666d6e06e03ea3b29
GelidElf <GelidElf@21b69b27-b653-a999-551f-f9debfb54786>
2012-09-04 21:25:03 +0000

    Cambios en el assembly station y conveyor belt para adecuarlos a las necesidadesd de los automatas

--- trunk/src/core/sections/AssembyStation/AssemblyStationInput.java
+++ trunk/src/core/sections/AssembyStation/AssemblyStationInput.java
@@ -1,9 +1,7 @@
 package core.sections.AssembyStation;
 
-
 public enum AssemblyStationInput {
 
-	START,
- ESTOP, RESTART, NSTOP, axisDetectedTrue, axisDetectedFalse, gearDetectedTrue, gearDetectedFalse, engage, apDetectedTrue, apDetectedFalse;
-	
+	START, ESTOP, RESTART, NSTOP, axisDetectedTrue, axisDetectedFalse, gearDetectedTrue, gearDetectedFalse, jobDone, apDetectedTrue, apDetectedFalse;
+
 }
