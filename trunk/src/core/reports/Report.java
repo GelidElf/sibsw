64048f35192c631585bc50939a213b33971da194
GelidElf <GelidElf@21b69b27-b653-a999-551f-f9debfb54786>
2012-09-05 18:58:12 +0000

    Lectura de reports y cambios en la vista de los mismos. Cambios en la interfaz

---
+++ trunk/src/core/reports/Report.java
@@ -0,0 +1,36 @@
+package core.reports;
+
+import java.util.HashMap;
+
+import core.messages.enums.ReportValues;
+
+public class Report {
+	
+	private HashMap<ReportValues, Integer> map = new HashMap<ReportValues, Integer>();
+	private HashMap<ReportValues, Integer> mapTotales = new HashMap<ReportValues, Integer>();
+		
+	public Report(){
+		for (ReportValues parameter : ReportValues.values()) {
+			map.put(parameter, 0);
+			mapTotales.put(parameter, 0);
+		}
+	}
+	
+	/**
+	 * Store report values
+	 */
+	private void receiveSignal(ReportValues signal) {
+		map.put(signal, map.get(signal) + 1);
+		mapTotales.put(signal, mapTotales.get(signal) + 1);
+	}
+	
+	public HashMap<ReportValues, Integer> getMap(){
+		return map;
+	}
+	
+	public HashMap<ReportValues, Integer> getMapTotales(){
+		return mapTotales;
+	}
+
+
+}
